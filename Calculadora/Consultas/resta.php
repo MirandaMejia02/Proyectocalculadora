5aa349235c23dbe2953579f0d983606c22d7345c
Miguel Miranda <127983121+MirandaMejia02@users.noreply.github.com>
2023-04-02 14:55:13 -0600

    Add files via upload

---
+++ Calculadora/Consultas/resta.php
@@ -0,0 +1,22 @@
+<?php
+class Resta
+{
+    public $n1;
+    public $n2;
+
+    public function __construct()
+    {
+    $this->n1 = $_POST['numero-1'];
+    $this->n2 = $_POST['numero-2'];
+    }
+
+    public function resta()
+    {
+        return $this->n1 - $this->n2;
+    }
+}
+$total = new resta();
+$resultado = $total->resta();
+echo "El resultado de la resta es: " . $resultado;
+
+?>
